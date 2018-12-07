f41aa9b9fa5fc67609606a4559efc21adc4b4452
Denis Peshekhonov <denis@peshekhonov.ru>
2018-12-07 03:45:26 +0300

    args & libraries

--- Slic3rSVG2PNG/Program.cs
+++ Slic3rSVG2PNG/Program.cs
@@ -1,15 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Globalization;
 
 namespace Slic3rSVG2PNG
 {
     class Program
     {
+        private static string   INPUT     = Path.GetFullPath("print.svg");
+        private static string   OUTPUT    = Path.GetFullPath("out");
+        private static double   PPU       = 0.0;
+
+        private static CultureInfo customCulture;
+
         static void Main(string[] args)
         {
+            ParseArgs(args);
+
+            //switch to international decimal separator
+            customCulture = (CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            Thread.CurrentThread.CurrentCulture = customCulture;
+
+            //log
+            Out("Program run with: ");
+            Out("...input file: " + INPUT);
+            Out("...output path: " + OUTPUT);
+            Out("...pixel per unit ratio: " + PPU);
+        }
+
+        private static void ParseArgs(string[] args)
+        {
+            for(int i = 0; i < args.Length - 1; i += 2)
+            {
+                string par = args[i];
+                string val = args[i + 1];
+
+                switch(par)
+                {
+                    case "--input":
+                        INPUT = Path.GetFullPath(val);
+                        break;
+                    case "--output":
+                        OUTPUT = Path.GetFullPath(val);
+                        break;
+                    case "--ppu":
+                        Out(val);
+                        PPU = double.Parse(val.Trim(), NumberStyles.AllowDecimalPoint, NumberFormatInfo.InvariantInfo);
+                        break;
+                }
+            }
+        }
+
+        public static void Out(string s)
+        {
+            Console.WriteLine(s);
         }
     }
 }
